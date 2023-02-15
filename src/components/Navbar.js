205c86b003a7bd9b044a334148e89507d73aac3f
anasirfan <39003284+anasirfan@users.noreply.github.com>
2023-02-16 02:49:27 +0500

    PATH CHANGES

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { RiGooglePlayLine } from "react-icons/ri";
-import { DarkLightSwitch } from "./NavbarComp/darkLightSwitch";
-import Notification from "./NavbarComp/Notification";
-import Profileicon from "./NavbarComp/Profileicon";
-import Searchbar from "./NavbarComp/Searchbar";
+import { DarkLightSwitch } from "../components/NavbarComp/DarkLightSwitch";
+import Notification from "../components/NavbarComp/Notification";
+import Profileicon from "../components/NavbarComp/Profileicon";
+import Searchbar from "../components/NavbarComp/Searchbar";
 const Navbar = () => {
   return (
       <div className="left-20">
