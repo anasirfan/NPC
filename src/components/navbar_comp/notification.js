4dc50253f44eb11a4acbc37d523f7cab5ec7aff4
anasirfan <39003284+anasirfan@users.noreply.github.com>
2023-03-06 03:10:32 +0500

    updates

--- src/components/navbar_comp/notification.js
+++ src/components/navbar_comp/notification.js
@@ -1,11 +1,93 @@
-import React from 'react';
-import { AiOutlineBell } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineBell, AiOutlineSetting } from "react-icons/ai";
+import { FiEye } from "react-icons/fi";
+
+import { MdClose } from "react-icons/md";
+import thumbnail1 from "./thumbnail1.jpg";
+import thumbnail2 from "./thumbnail2.jpg";
+import Image from "next/image";
+
+const notifications = [
+  {
+    id: 1,
+    message: "CryptoGuy is going live: Birthday livestream",
+    time: "5 hours ago",
+    thumbnail: thumbnail1,
+  },
+  {
+    id: 2,
+    message: "New video: How to use React Hooks",
+    time: "6 hours ago",
+    thumbnail: thumbnail2,
+  },
+];
+
 const Notification = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleBellClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleNotificationClose = (id) => {
+    // Remove the notification with the given id
+    setNotifications((notifications) =>
+      notifications.filter((n) => n.id !== id)
+    );
+  };
+
   return (
-    <div className="ml-18">
-      <button className="p-2 rounded-full bg-gray-200">
+    <div className="relative inline-block ml-18">
+      <button
+        className="p-2 rounded-full bg-gray-200"
+        onClick={handleBellClick}
+      >
         <AiOutlineBell className="text-gray-600" />
       </button>
+      {isOpen && (
+        <div className="absolute right-0 mt-2 w-96 bg-white rounded-lg shadow-lg">
+          <div className="px-4 py-2 flex justify-between items-center">
+            <h3 className="font-semibold text-lg">Notifications</h3>
+            <button onClick={handleBellClick}>
+              <AiOutlineSetting className="text-gray-400 w-6 h-6 mt-2 hover:text-gray-800" />
+            </button>
+          </div>
+          <ul>
+            {notifications.map((n) => (
+              <li key={n.id} className="border-b border-gray-200 px-4 py-3">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center w-96 space-x-4">
+                    
+                    
+                    <div className="pr-4">
+                      <p className="font-sm font-semibold">{n.message}</p>
+                      <p className="text-gray-500 text-xs">{n.time}</p>
+                    </div>
+                    <Image
+                    src={n.thumbnail}
+                    alt="Thumbnail"
+                    className="w-32 h-10 mb-8 object-cover pr-7 mr-8"
+                    />
+                  </div>
+                  <div className="flex items-center">
+                    <button onClick={() => handleNotificationClose(n.id)}>
+                      <AiOutlineBell className="text-gray-500 mb-6 h-4 w-4 hover:text-gray-700 mr-4" />
+                    </button>
+                    <button onClick={() => handleNotificationClose(n.id)}>
+                      <FiEye className="mb-6" />
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+          {notifications.length === 0 && (
+            <div className="px-4 py-2 text-center">
+              <p className="text-gray-500">No notifications</p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,28 +1,28 @@
 //h-37px py-4 px-4
-import { useState } from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
-import { BiHome } from 'react-icons/bi';
-import { AiOutlineFire } from 'react-icons/ai';
-import { MdOutlineLiveTv } from 'react-icons/md';
-import { IoMdNotificationsOutline } from 'react-icons/io';
-import { AiOutlinePlaySquare } from 'react-icons/ai';
-import { AiOutlineHistory } from 'react-icons/ai';
-import { FaRegHospital } from 'react-icons/fa';
-import { WiStars } from 'react-icons/wi';
-import { MdOutlineBusinessCenter } from 'react-icons/md';
-import { BsController } from 'react-icons/bs';
-import { ImMusic } from 'react-icons/im';
-import { MdOutlineTheaterComedy } from 'react-icons/md';
-import { CiTrophy } from 'react-icons/ci';
-import { TfiStatsUp } from 'react-icons/tfi';
-import { TfiPencilAlt } from 'react-icons/tfi';
-import { FaPaw } from 'react-icons/fa';
-import { AiOutlineVideoCamera } from 'react-icons/ai';
-import { BsCamera } from 'react-icons/bs';
-import { AiOutlineCar } from 'react-icons/ai';
-import { MdOutlineCardTravel } from 'react-icons/md';
-import styles from './sidebar.module.css';
-
+import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { BiHome } from "react-icons/bi";
+import { AiOutlineFire } from "react-icons/ai";
+import { MdOutlineLiveTv } from "react-icons/md";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { AiOutlinePlaySquare } from "react-icons/ai";
+import { AiOutlineHistory } from "react-icons/ai";
+import { FaRegHospital } from "react-icons/fa";
+import { WiStars } from "react-icons/wi";
+import { MdOutlineBusinessCenter } from "react-icons/md";
+import { BsController } from "react-icons/bs";
+import { ImMusic } from "react-icons/im";
+import { MdOutlineTheaterComedy } from "react-icons/md";
+import { CiTrophy } from "react-icons/ci";
+import { TfiStatsUp } from "react-icons/tfi";
+import { TfiPencilAlt } from "react-icons/tfi";
+import { FaPaw } from "react-icons/fa";
+import { AiOutlineVideoCamera } from "react-icons/ai";
+import { BsCamera } from "react-icons/bs";
+import { AiOutlineCar } from "react-icons/ai";
+import { MdOutlineCardTravel } from "react-icons/md";
+import styles from "./sidebar.module.css";
+import TVIcon from "./icons/tv-icon";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -31,134 +31,214 @@ const Sidebar = () => {
     setIsOpen(!isOpen);
   };
   return (
-    
-      <div className="sidebar-container h-37px py-4 px-4">
-        <div className="menu-icon " onClick={toggleSidebar}>
-          <AiOutlineMenu size={23} style={{ marginBottom: '50px' }} />
+    <div className="sidebar-container h-37px py-4 px-4" style={{minWidth: isOpen?  'max-content': 'auto'}}>
+      <div className="menu-icon " onClick={toggleSidebar}>
+        <AiOutlineMenu size={23} style={{ marginBottom: "50px" }} />
+      </div>
+      <div className={`sidebar ${isOpen ? "sidebar-open" : ""}`}>
+        <div className="sidebar-icons  mb-2">
+          <BiHome
+            size={23}
+            style={{ marginRight: "10px", display: "inline-block" }}
+          />
+          <span style={{ display: "inline-block", alignItems: "center" }}>
+            {isOpen && "Home"}
+          </span>
         </div>
-        <div className={`sidebar ${isOpen ? 'sidebar-open' : ''}`}>
-          <div className="sidebar-icons  mb-2">
-            <BiHome size={23} style={{ marginRight: '10px' }} />
-            <span style={{ display: 'flex', alignItems: 'center' }}>
-              {isOpen && 'Home'}
+        <div className="sidebar-icons mb-2">
+          <AiOutlineFire
+            size={23}
+            style={{ marginRight: "10px", display: "inline-block" }}
+          />
+          <span style={{ display: "inline-block", alignItems: "center" }}>
+            {isOpen && "Trending"}
+          </span>
+        </div>
+        <div className="sidebar-icons mb-2">
+          <TVIcon
+            size={23}
+            style={{
+              marginRight: "10px",
+              display: "inline-block",
+              marginBottom: "20px",
+            }}
+          />
+          <span
+            style={{
+              display: "inline-block",
+              alignItems: "center",
+              opacity: isOpen ? "1" : "0",
+            }}
+          >
+            Live TV
+          </span>
+
+          <div className="sidebar-icons mb-2">
+            <IoMdNotificationsOutline
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Notifications"}
             </span>
           </div>
           <div className="sidebar-icons mb-2">
-            <AiOutlineFire size={23} style={{ marginRight: '10px' }} />
-            <span style={{ display: 'flex', alignItems: 'center' }}>
-              {isOpen && 'Trending'}
+            <AiOutlinePlaySquare
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Videos"}
             </span>
           </div>
           <div className="sidebar-icons mb-2">
-            <MdOutlineLiveTv size={23} style={{ marginRight: '10px' , marginBottom: '20px' }} />
+            <AiOutlineHistory
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
             <span
               style={{
-                display: 'flex',
-                alignItems: 'center',
-                opacity: isOpen ? '1' : '0',
-                
+                display: "inline-block",
+                alignItems: "center",
+                marginBottom: "40px",
               }}
             >
-              Live TV
+              {isOpen && "History"}
             </span>
-
-        <div className="sidebar-icons mb-2">
-          <IoMdNotificationsOutline size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Notifications'}
-          </span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <AiOutlinePlaySquare size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Videos'}
-          </span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <AiOutlineHistory size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' , marginBottom: '40px' }}>
-            {isOpen && 'History'}
-          </span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <FaRegHospital size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Notifications'}
-          </span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <WiStars size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Favorites'}
-          </span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <MdOutlineBusinessCenter size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Business'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <BsController size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Games'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <ImMusic size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Music'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <MdOutlineTheaterComedy size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Movies'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <CiTrophy size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Sports'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <TfiStatsUp size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Learning'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <TfiPencilAlt size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Create'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <FaPaw size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Animals'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <AiOutlineVideoCamera size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Shorts'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <BsCamera size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && '360° Video'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <AiOutlineCar size={23} style={{ marginRight: '10px' }}/>
-          <span style={{ display: 'flex', alignItems: 'center' }}>
-            {isOpen && 'Automotive'}</span>
-        </div>
-        <div className="sidebar-icons mb-2">
-          <MdOutlineCardTravel size={23} style={{ marginRight: '10px' }}/>
-          <span
+          </div>
+          <div className="sidebar-icons mb-2">
+            <FaRegHospital
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Notifications"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <WiStars
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Favorites"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <MdOutlineBusinessCenter
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Business"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <BsController
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Games"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <ImMusic
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Music"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <MdOutlineTheaterComedy
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Movies"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <CiTrophy
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Sports"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <TfiStatsUp
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Learning"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <TfiPencilAlt
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Create"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <FaPaw
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Animals"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <AiOutlineVideoCamera
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Shorts"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <BsCamera
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "360° Video"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <AiOutlineCar
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span style={{ display: "inline-block", alignItems: "center" }}>
+              {isOpen && "Automotive"}
+            </span>
+          </div>
+          <div className="sidebar-icons mb-2">
+            <MdOutlineCardTravel
+              size={23}
+              style={{ marginRight: "10px", display: "inline-block" }}
+            />
+            <span
               style={{
-                display: 'flex',
-                alignItems: 'center',
-                opacity: isOpen ? '1' : '0',
-                transition: 'opacity 0.3s ease-in-out',
+                display: "inline-block",
+                alignItems: "center",
+                opacity: isOpen ? "1" : "0",
+                transition: "opacity 0.3s ease-in-out",
               }}
             >
-            {isOpen && 'Travel'}</span>
-        </div>
+              {isOpen && "Travel"}
+            </span>
+          </div>
         </div>
       </div>
     </div>
